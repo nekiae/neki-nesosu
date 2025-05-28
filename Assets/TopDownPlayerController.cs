bad471e194596cc756021410348609dbb27b031f
LightMizu <1blend.your.life1@gmail.com>
2025-05-29 01:49:32 +0300

    Animation in code

--- Assets/TopDownPlayerController.cs
+++ Assets/TopDownPlayerController.cs
@@ -26,22 +26,27 @@ public class TopDownPlayerController : MonoBehaviour
 
     private Vector2 moveInput;
     private Vector2 currentVelocity;
-    
+
+
+    private SpriteRenderer sprite;
     private Rigidbody2D rb;
 
     private TextMeshProUGUI pressE;
 
     private PlayerInput playerInput;
 
+    private Animator animator;
     public void Start()
     {
         // Get required components
+        sprite = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         pressE = textUI.GetComponent<TextMeshProUGUI>();
         playerInput = FindFirstObjectByType<PlayerInput>();
         navigationAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
 
-        //Setup navigationAgent
+        //Setup navigation agent
         navigationAgent.updateRotation = false;
         navigationAgent.updateUpAxis = false;
 
@@ -50,7 +55,10 @@ public class TopDownPlayerController : MonoBehaviour
     public void FixedUpdate()
     {
         HandleMovement();
+        HandelAnimator();
     }
+
+
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
@@ -65,9 +73,45 @@ public class TopDownPlayerController : MonoBehaviour
             playerInput.enabled = false;
             pressE.SetText("");
             StartCoroutine(Interaction());
-            
-            
+
+
         }
+    }
+
+    public void HandelAnimator()
+    {
+        int x;
+        int y;
+        if (navigationAgent.velocity != Vector3.zero)
+        {
+            x = System.Math.Sign(navigationAgent.velocity.x);
+            y = System.Math.Sign(navigationAgent.velocity.y);
+        }
+
+        else
+        {
+            x = System.Math.Sign(rb.linearVelocityX);
+            y = System.Math.Sign(rb.linearVelocityY);
+        }
+
+
+        if (y == 0)
+        {
+            if (x < 0)
+            {
+                sprite.flipX = true;
+            }
+            else if (x > 0)
+            {
+                sprite.flipX = false;
+            }
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
+        animator.SetInteger("x", x);
+        animator.SetInteger("y", y);
     }
 
     void HandleMovement()
@@ -105,7 +149,8 @@ public class TopDownPlayerController : MonoBehaviour
         }
     }
 
-    IEnumerator Interaction(){
+    IEnumerator Interaction()
+    {
         yield return StartCoroutine(Move(tableCoord));
         dialogController.ShowDialog("Вочы зачыняются...");
     }
@@ -116,12 +161,12 @@ public class TopDownPlayerController : MonoBehaviour
         while (navigationAgent.velocity == Vector3.zero)
         {
             yield return new WaitForFixedUpdate();
-        } 
+        }
         while (navigationAgent.velocity != Vector3.zero)
         {
             yield return new WaitForFixedUpdate();
-        } 
-        
+        }
+
     }
 
 }
